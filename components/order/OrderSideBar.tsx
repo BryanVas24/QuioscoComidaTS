13238f6375491fe61bf276f2d57752e39bce87b5
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-22 21:20:35 -0600

    Obteniendo categorias de alimentos y evitando multiples instancias de prismaClient

--- components/order/OrderSideBar.tsx
+++ components/order/OrderSideBar.tsx
@@ -1,4 +1,13 @@
-const OrderSideBar = () => {
+import { prisma } from "@/src/libs/prisma";
+
+//función para traer todas las categorias
+async function getCategories() {
+  return await prisma.category.findMany();
+}
+//caracteristica de next, podes tener componentes asincronos
+const OrderSideBar = async () => {
+  const categories = await getCategories();
+
   return <aside className="md:w-72 md:h-screen bg-white">OrderSideBar</aside>;
 };
 
