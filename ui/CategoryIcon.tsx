31c40c1f443b184869fff582cd61c6684078baa5
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-23 14:10:38 -0600

    Resaltando la categoria en la que se encuentra

--- ui/CategoryIcon.tsx
+++ ui/CategoryIcon.tsx
@@ -1,15 +1,22 @@
+"use client";
 import { Category } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 type CategoryIconProps = {
   CategoryInfo: Category;
 };
 function CategoryIcon({ CategoryInfo }: CategoryIconProps) {
   const { name, slug } = CategoryInfo;
+
+  const params = useParams<{ category: string }>();
+
   return (
     <div
-      className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
+      className={`${
+        slug === params.category ? "bg-amber-400" : ""
+      } flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
     >
       <div className="w-16 h-16 relative">
         <Image src={`/icon_${slug}.svg`} fill alt={`imagen de ${name}`} />
