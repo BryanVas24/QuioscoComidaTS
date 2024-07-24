447665a3da139375ec57a7f3a3769c9d7de86171
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-24 12:26:44 -0600

    Mostrando el total de la orden

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,12 +1,19 @@
 "use client";
 import { useStore } from "@/src/store";
 import ProductDetails from "./ProductDetails";
+import { useMemo } from "react";
+import { formatCurrency } from "@/src/utils";
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order);
 
+  const total = useMemo(
+    () => order.reduce((total, item) => total + item.quantity * item.price, 0),
+    [order]
+  );
+
   return (
-    <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
+    <aside className="md:h-screen md:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi pedido</h1>
       {order.length === 0 ? (
         <p className="text-center my-10">El carrito esta vacio</p>
@@ -15,6 +22,10 @@ export default function OrderSummary() {
           {order.map((item) => (
             <ProductDetails key={item.id} item={item} />
           ))}
+          <p className="text-2xl mt-20 text-center">
+            Total a pagar:{" "}
+            <span className="font-bold">{formatCurrency(total)}</span>
+          </p>
         </div>
       )}
     </aside>
