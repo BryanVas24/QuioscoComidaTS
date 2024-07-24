c61cf1e5f6a8a897e832a80ea6dc20e17e96a40f
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-24 11:42:48 -0600

    Evitando duplicados

--- src/store.ts
+++ src/store.ts
@@ -6,21 +6,34 @@ interface Store {
   order: OrderItem[];
   addToOrder: (product: Product) => void;
 }
-export const useStore = create<Store>((set) => ({
+export const useStore = create<Store>((set, get) => ({
   order: [],
   addToOrder: (product) => {
     //esto es para omitir datos
     const { categoryId, image, ...data } = product;
-    //set te permite escribir en el state
-    set((state) => ({
-      order: [
-        ...state.order,
+    let order: OrderItem[] = [];
+    if (get().order.find((item) => item.id === product.id)) {
+      order = get().order.map((item) =>
+        item.id === product.id
+          ? {
+              ...item,
+              quantity: item.quantity + 1,
+            }
+          : item
+      );
+    } else {
+      order = [
+        ...get().order,
         {
           ...data,
           quantity: 1,
           subtotal: 1 * product.price,
         },
-      ],
+      ];
+    }
+    //set te permite escribir en el state
+    set((state) => ({
+      order,
     }));
   },
 }));
