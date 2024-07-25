8775c35d4f0fe904ad7e5435e14b0c4c73a29469
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-25 12:53:22 -0600

    Evitando duplicar ordenes

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -9,6 +9,7 @@ import { toast } from "react-toastify";
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order);
+  const clearOrder = useStore((state) => state.clearOrder);
 
   const total = useMemo(
     () => order.reduce((total, item) => total + item.quantity * item.price, 0),
@@ -36,6 +37,9 @@ export default function OrderSummary() {
         toast.error(issue.message);
       });
     }
+
+    toast.success("Pedido realizado correctamente");
+    clearOrder();
   };
   return (
     <aside className="md:h-screen md:overflow-y-scroll md:w-64 lg:w-96 p-5">
