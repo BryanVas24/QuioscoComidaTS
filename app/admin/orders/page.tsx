524291c86f501583c7c6594128a21233780ed461
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-26 15:27:39 -0600

    Tomando Ordenes y mostrandolas

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,9 +1,37 @@
+import Ordercards from "@/components/order/Ordercards";
+import { prisma } from "@/src/libs/prisma";
 import Heading from "@/ui/Heading";
 
-function OrdersPage() {
+async function getPendingOrders() {
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false,
+    },
+    include: {
+      orderProducts: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+  return orders;
+}
+async function OrdersPage() {
+  const orders = await getPendingOrders();
+
   return (
     <>
       <Heading>Administrar ordenes</Heading>
+      {orders.length ? (
+        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+          {orders.map((order) => (
+            <Ordercards key={order.id} order={order} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-center">No hay Ordenes pendientes</p>
+      )}
     </>
   );
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,13 @@
-import { Product } from "@prisma/client";
+import { Order, OrderProducts, Product } from "@prisma/client";
 
 //de esta manera haces pick y agregas más atributos
 export type OrderItem = Pick<Product, "id" | "name" | "price"> & {
   quantity: number;
   subtotal: number;
+};
+
+export type OrderWithProducts = Order & {
+  orderProducts: (OrderProducts & {
+    product: Product;
+  })[];
 };
