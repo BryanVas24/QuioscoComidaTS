5e534e7656cc9f0ffccb44fc824ebfde4e122ad3
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-28 11:33:18 -0600

    Creando paginador sin terminar al 100%

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,16 +2,27 @@ import ProductTable from "@/components/products/ProsuctTable";
 import { prisma } from "@/src/libs/prisma";
 import Heading from "@/ui/Heading";
 
-async function getproducts() {
+async function getproducts(page: number, pageSize: number) {
+  const skip = (page - 1) * pageSize;
   const products = await prisma.product.findMany({
+    //take te dice cuantos registros traer
+    take: pageSize,
+    //skip cuantos registros debe sltarse
+    skip,
     include: {
       category: true,
     },
   });
   return products;
 }
-export default async function page() {
-  const products = await getproducts();
+export default async function page({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  const page = +searchParams.page || 1;
+  const pageSize = 10;
+  const products = await getproducts(page, pageSize);
 
   return (
     <>
