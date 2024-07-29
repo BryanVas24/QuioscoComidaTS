0b0e3bbe42a42229cc4eb6398c856110a9db2653
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-29 16:06:44 -0600

    Paginador terminado

--- components/products/ProductsPagination.tsx
+++ components/products/ProductsPagination.tsx
@@ -8,6 +8,7 @@ export default function ProductsPagination({
   page,
   totalPages,
 }: ProductsPaginationProps) {
+  const numbersInPage = Array.from({ length: totalPages }, (_, i) => i + 1);
   return (
     <nav className="flex justify-center py-10">
       {page > 1 && (
@@ -18,6 +19,17 @@ export default function ProductsPagination({
           &laquo;
         </Link>
       )}
+      {numbersInPage.map((currentPage) => (
+        <Link
+          className={`${
+            page === currentPage ? "bg-amber-400" : "bg-white "
+          } px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-8`}
+          key={currentPage}
+          href={`/admin/products?page=${currentPage}`}
+        >
+          {currentPage}
+        </Link>
+      ))}
       {page < totalPages && (
         <Link
           className="bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-8"
