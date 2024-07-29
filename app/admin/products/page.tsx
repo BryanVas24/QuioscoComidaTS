3e7c2590f508959b96e6110d7a62bcc9aae7ced0
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-29 16:19:27 -0600

    Creando un formulario de busqueda de productos y boton para agregar productos

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,7 +1,9 @@
+import ProductSearchForm from "@/components/products/ProductSearchForm";
 import ProductsPagination from "@/components/products/ProductsPagination";
 import ProductTable from "@/components/products/ProsuctTable";
 import { prisma } from "@/src/libs/prisma";
 import Heading from "@/ui/Heading";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 async function productCount() {
@@ -47,6 +49,15 @@ export default async function page({
   return (
     <>
       <Heading>Administrar productos</Heading>
+      <div className="flex flex-col lg:flex-row lg:justify-evenly gap-5">
+        <Link
+          href={`/admin/products/new`}
+          className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
+        >
+          Crear Producto
+        </Link>
+        <ProductSearchForm />
+      </div>
       <ProductTable products={products} />
       <ProductsPagination page={page} totalPages={totalPages} />
     </>
