67018fa358e01a5c0b1da6cfb56052ee1a2188b0
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-30 21:26:28 -0600

    Agregando componentes de cliente y servidor

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -1,11 +1,14 @@
 import AddProductForm from "@/components/products/AddProductForm";
+import ProductForm from "@/components/products/ProductForm";
 import Heading from "@/ui/Heading";
-
+//lo de abajo es para poder mezclar componentes del cliente con componentes del servidor
 export default function page() {
   return (
     <div>
       <Heading>Nuevo Producto</Heading>
-      <AddProductForm />
+      <AddProductForm>
+        <ProductForm />
+      </AddProductForm>
     </div>
   );
 }
--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -1,10 +1,13 @@
+"use client";
+
 import ProductForm from "./ProductForm";
 
-function AddProductForm() {
+function AddProductForm({ children }: { children: React.ReactNode }) {
+  const handleSubmit = async (formData: FormData) => {};
   return (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-3xl mx-auto">
-      <form className="space-y-5">
-        <ProductForm />
+      <form className="space-y-5" action={handleSubmit}>
+        {children}
         <input
           type="submit"
           value={"Registrar Producto"}
