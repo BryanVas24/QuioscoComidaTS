b8eafc2cd6669ea7398f75acafbe3e8d914a63c3
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-30 21:33:27 -0600

    Validando la creación de productos

--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -1,9 +1,24 @@
 "use client";
 
-import ProductForm from "./ProductForm";
+import { ProductSchema } from "@/src/schema";
+
+import { toast } from "react-toastify";
 
 function AddProductForm({ children }: { children: React.ReactNode }) {
-  const handleSubmit = async (formData: FormData) => {};
+  const handleSubmit = async (formData: FormData) => {
+    const data = {
+      name: formData.get("name"),
+      price: formData.get("price"),
+      categoryId: formData.get("categoryId"),
+    };
+    const result = ProductSchema.safeParse(data);
+    if (!result.success) {
+      result.error.issues.forEach((issue) => {
+        toast.error(issue.message);
+      });
+      return;
+    }
+  };
   return (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-3xl mx-auto">
       <form className="space-y-5" action={handleSubmit}>
