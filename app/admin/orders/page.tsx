4d69ae15cc8021dcf441c51a7c6ac74cb1c52118
BryanVas24 <bryanvasquez2406@gmail.com>
2024-08-01 18:48:53 -0600

    Configurando swr

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,52 +1,39 @@
+"use client";
 import Ordercards from "@/components/order/Ordercards";
 import { prisma } from "@/src/libs/prisma";
+import { OrderWithProducts } from "@/src/types";
 import Heading from "@/ui/Heading";
 import { revalidatePath } from "next/cache";
+import useSWR from "swr";
 
-async function getPendingOrders() {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false,
-    },
-    include: {
-      orderProducts: {
-        include: {
-          product: true,
-        },
-      },
-    },
+function OrdersPage() {
+  const url = "/admin/orders/api";
+  const fetcher = () =>
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => data);
+
+  const { data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+    refreshInterval: 60000,
+    revalidateOnFocus: false,
   });
-  return orders;
-}
-async function OrdersPage() {
-  const orders = await getPendingOrders();
+  if (isLoading) return "Cargando...";
+  if (data)
+    return (
+      <>
+        <Heading>Administrar ordenes</Heading>
 
-  return (
-    <>
-      <Heading>Administrar ordenes</Heading>
-      <form
-        action={async () => {
-          "use server";
-          revalidatePath("/admin/orders");
-        }}
-      >
-        <input
-          className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
-          type="submit"
-          value={"Actualizar ordenes"}
-        />
-      </form>
-      {orders.length ? (
-        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-          {orders.map((order) => (
-            <Ordercards key={order.id} order={order} />
-          ))}
-        </div>
-      ) : (
-        <p className="text-center">No hay Ordenes pendientes</p>
-      )}
-    </>
-  );
+        {data.length ? (
+          <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+            {data.map((order) => (
+              <Ordercards key={order.id} order={order} />
+            ))}
+          </div>
+        ) : (
+          <p className="text-center">No hay Ordenes pendientes</p>
+        )}
+      </>
+    );
 }
 
 export default OrdersPage;
